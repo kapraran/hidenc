082bd47172f2643070fd3b5af20fe1e8deb2f882
Nikolaos Kapraras <iamnikosk93@gmail.com>
2019-08-01 19:24:34 +0300

    minor fixes

--- src/hidenc.js
+++ src/hidenc.js
@@ -13,5 +13,7 @@ program
   .action(function(file, key) {
     if (this.encrypt) return encrypt.call(this, file, key)
     if (this.decrypt) return decrypt.call(this, file, key)
+
+    console.error("ERROR: Missing arguments")
   })
   .parse(process.argv)
